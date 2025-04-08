31cf843be1748801f33dee9c509ce524905cd714
loveCamille <343235900@qq.com>
2025-04-08 17:24:11 +0800

    新增冻结异常

--- src/main/java/com/taylor/common/web/exception/FrozenException.java
+++ src/main/java/com/taylor/common/web/exception/FrozenException.java
@@ -1,7 +1,5 @@
 package com.taylor.common.web.exception;
 
-import com.taylor.common.web.constant.MessageConstant;
-
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -13,9 +11,8 @@ import java.util.Date;
  */
 public class FrozenException extends RuntimeException {
 
-    public FrozenException(Date expiredAfter) {
-        super(String.format(MessageConstant.FROZEN_MESSAGE,
-                new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(expiredAfter)));
+    public FrozenException(String message, Date expiredAfter) {
+        super(message + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(expiredAfter));
     }
 
 }
