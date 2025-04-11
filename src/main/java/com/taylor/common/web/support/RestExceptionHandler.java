e8ff09d341d246bb54b44a139c3ae52ac1c39572
loveCamille <343235900@qq.com>
2025-04-11 16:45:35 +0800

    新增捕获校验异常

--- src/main/java/com/taylor/common/web/support/RestExceptionHandler.java
+++ src/main/java/com/taylor/common/web/support/RestExceptionHandler.java
@@ -4,8 +4,11 @@ import com.taylor.common.web.exception.BizException;
 import com.taylor.common.web.util.MDCUtil;
 import com.taylor.common.web.domain.Result;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Objects;
 
 /**
  * 接口全局异常处理
@@ -17,8 +20,13 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class RestExceptionHandler {
 
+    @ExceptionHandler(BindException.class)
+    public Result<Void> validException(BindException e) {
+        return Result.fail(Objects.requireNonNull(e.getBindingResult().getFieldError()).getDefaultMessage());
+    }
+
     @ExceptionHandler(BizException.class)
-    public Result<Void> exception(BizException e) {
+    public Result<Void> bizException(BizException e) {
         return Result.fail(e.getMessage());
     }
 
