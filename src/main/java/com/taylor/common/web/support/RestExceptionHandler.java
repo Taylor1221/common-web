30df4fb9bbd47fd14031faed9a36373f820bcc3d
loveCamille <343235900@qq.com>
2025-04-11 17:05:45 +0800

    新增捕获HttpRequestMethodNotSupportedException

--- src/main/java/com/taylor/common/web/support/RestExceptionHandler.java
+++ src/main/java/com/taylor/common/web/support/RestExceptionHandler.java
@@ -5,6 +5,7 @@ import com.taylor.common.web.util.MDCUtil;
 import com.taylor.common.web.domain.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.BindException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -23,6 +24,11 @@ public class RestExceptionHandler {
     @ExceptionHandler(BindException.class)
     public Result<Void> validException(BindException e) {
         return Result.fail(Objects.requireNonNull(e.getBindingResult().getFieldError()).getDefaultMessage());
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public Result<?> handleMethodNotSupported(HttpRequestMethodNotSupportedException ex) {
+        return Result.fail("不支持的请求方式 " + ex.getMethod());
     }
 
     @ExceptionHandler(BizException.class)
